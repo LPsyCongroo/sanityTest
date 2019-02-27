6a735b33420dbdf8d793585b90e7868d6f1f62a6
Ali Rehmatullah <alirehmatullah@dhcp-wifi-8021x-155-41-20-21.bu.edu>
2019-02-26 22:13:58 -0500

    fix netlify deploy

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -11,8 +11,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-sanity',
       options: {
-        projectId,
-        dataset,
+        projectId: 'nz86xmtk',
+        dataset: 'production',
         // To enable preview of drafts, copy .env-example into .env,
         // and add a token with read permissions
         token: process.env.SANITY_TOKEN,
